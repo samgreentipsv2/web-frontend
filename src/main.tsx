bba3a321b590a433de972aab9370ba4f20b6afac
ceejaycodes <dreelceejay79@yahoo.com>
2023-04-28 09:47:24 +0100

    removed dist folder to allow vercel to generate

--- src/main.tsx
+++ src/main.tsx
@@ -76,7 +76,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <AuthProvider authType = {'cookie'}
                   authName={'_auth'}
                   cookieDomain={window.location.hostname}
-                  cookieSecure={true}
+                  cookieSecure={window.location.protocol === "https:"}
                   >
        <RouterProvider router={Router} />
        </AuthProvider>
